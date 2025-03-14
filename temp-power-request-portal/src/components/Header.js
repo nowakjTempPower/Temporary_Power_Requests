ed732357d266889d39415086a358d5d08aa85691
nowakjTempPower <Jamie.nowak@southwire.com>
2025-03-14 12:25:54 -0400

    Fix image path for Southwire logo

--- temp-power-request-portal/src/components/Header.js
+++ temp-power-request-portal/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <img src="/southwire-logo.png" alt="Southwire Logo" />
+    <img src={`${process.env.PUBLIC_URL}/southwire-logo.png`} alt="Southwire Logo" />
     <h1>Temporary Power Team Request Portal</h1>
   </header>
 );
