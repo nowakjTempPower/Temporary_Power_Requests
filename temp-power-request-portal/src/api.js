56fb198f302d770b56a2bbe82517dd7e7c4872ef
nowakjTempPower <Jamie.nowak@southwire.com>
2025-03-25 07:56:15 -0400

    Updated API URL to Zapier webhook

--- temp-power-request-portal/src/api.js
+++ temp-power-request-portal/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Power Automate Flow URL (replace with actual trigger URL)
-const POWER_AUTOMATE_URL = "https://prod-10.westus.logic.azure.com:443/workflows/e5926c34738e46bf9408dca6c47f8ddb/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=xIHwo4upYs7iK6IF0YwpuiRxSSutG_qiiIUO9YV3qo4"; // Replace with real URL
+const POWER_AUTOMATE_URL = "https://hooks.zapier.com/hooks/catch/22193095/2e80oum/"; // Replace with real URL
 
 const SHAREPOINT_UPLOAD_URL = "https://southwire.sharepoint.com/:x:/s/YoungsvilleOperations268/EXQQAQ8byeRHno1vBjB1z6UB4AEgzWtLjR80tMY8cHBJRQ?e=jKe63R"; // Replace with real SharePoint upload API
 
